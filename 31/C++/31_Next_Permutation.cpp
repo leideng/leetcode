64dd4e234dfde4f3d89d6fc68281d41b2d3f2977
Lei Deng <ldeng.sjtu@gmail.com>
2016-06-17 13:51:54 +0800

    modify some comments in 31

--- 31/C++/31_Next_Permutation.cpp
+++ 31/C++/31_Next_Permutation.cpp
@@ -7,9 +7,9 @@ If such arrangement is not possible, it must rearrange it as the lowest possible
 The replacement must be in-place, do not allocate extra memory.
 
 Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
-1,2,3 ¡ú 1,3,2
-3,2,1 ¡ú 1,2,3
-1,1,5 ¡ú 1,5,1
+1,2,3 -> 1,3,2
+3,2,1 -> 1,2,3
+1,1,5 -> 1,5,1
 
 */
 
@@ -17,7 +17,7 @@ Here are some examples. Inputs are in the left-hand column and its corresponding
 
     1 From the last element to the first element, find the position that we want to increase 
       The condition is the first time we find  an i such that nums[i+1] > nums[i]. 
-    2 If we can find the position, we increase nums[i-1] and sort the remaining elements
+    2 If we can find the position, we increase nums[i] and sort the remaining elements
     3 If we cannot find the position, we come to the last permutation, and we just sort to come to the first permutation
     4 We should make use of the fact [i+1,i+2, ..., n-1] has already been in a descending order. 
       We can then just reverse the sub-vector to do sorting.
