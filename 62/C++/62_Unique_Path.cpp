b1bbe885e5be8d62e7ccb66bdcdd8c0e24f61652
Lei Deng <ldeng.sjtu@gmail.com>
2016-06-29 17:19:20 +0800

    finsh 62 Unique Path

---
+++ 62/C++/62_Unique_Path.cpp
@@ -0,0 +1,61 @@
+/** Problem
+
+A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+
+The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+
+How many possible unique paths are there?
+
+*/
+
+/** Algorithm Outline
+
+*/
+
+
+/**  Recursion, time limit exceeded 
+class Solution 
+{
+    public:
+    
+    int uniquePaths(int m, int n) 
+    {
+        if(m==1 || n == 1) 
+        {
+            return 1;
+        }
+        else
+        {
+            return uniquePaths(m,n-1) + uniquePaths(m-1,n);
+        }
+    }
+};
+
+*/
+
+class Solution 
+{
+    public:
+    
+    int uniquePaths(int m, int n) 
+    {
+        vector< vector<int> > nums(m, vector<int>(n, 0));
+        
+        for(int i=0; i<m; ++i)
+        {
+            for(int j=0; j<n; j++)
+            {
+                if(i==0 || j == 0)
+                {
+                    nums[i][j] = 1;
+                }
+                else
+                {
+                    nums[i][j] = nums[i-1][j] + nums[i][j-1];
+                }                    
+            }
+        }
+        
+        return nums[m-1][n-1];
+    }
+};
